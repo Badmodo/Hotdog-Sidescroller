e4e3fb6f260b3ca6a8dbd874fadd805e026b8c8a
HiryuJupiter <xooase@gmail.com>
2021-02-11 16:25:27 +1100

    enemy movement

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -6,29 +6,41 @@ using UnityEngine.SceneManagement;
 public class EnemyMove : MonoBehaviour
 {
     public int EnemySpeed;
-    public int XMoveDirection;
+    public bool facingRight = false;
+
+    SpriteRenderer sr;
+    int moveDir;
+
+    void Start()
+    {
+        UpdateMoveDir();
+
+        sr = GetComponent<SpriteRenderer>();
+    }
+
 
     void Update()
     {
         CollisionDetection();
         MoveForward();
+        UpdateFacing();
     }
 
     void CollisionDetection ()
     {
-        Vector2 checkDir = new Vector2(XMoveDirection, 0);
+        Vector2 checkDir = new Vector2(moveDir, 0);
         RaycastHit2D hit = Physics2D.Raycast(transform.position, checkDir);
-        Debug.DrawRay(transform.position, new Vector2(XMoveDirection, 0), Color.red);
+        Debug.DrawRay(transform.position, new Vector2(moveDir, 0), Color.red);
 
         if (hit && hit.distance < 0.6f)
         {
-            if (hit.collider.tag == "Ground")
-            {
-                Flip();
-            }
-            else if (hit.collider.tag == "Player")
+            if (hit.collider.tag == "Player")
             {
                 HitsPlayerCollider(hit.collider);
+            }
+            else
+            {
+                Flip();
             }
         }
     }
@@ -43,18 +55,36 @@ public class EnemyMove : MonoBehaviour
 
     void MoveForward ()
     {
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveDir, 0) * EnemySpeed;
     }
    
     void Flip ()
     {
-        if (XMoveDirection > 0)
+        facingRight = !facingRight;
+        UpdateMoveDir();
+    }
+
+    void UpdateFacing ()
+    {
+        sr.flipX = facingRight;
+    }
+
+    void UpdateMoveDir ()
+    {
+        if (facingRight)
         {
-            XMoveDirection = -1;
+            moveDir = 1;
         }
         else
         {
-            XMoveDirection = 1;
+            moveDir = -1;
         }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(20, 20, 200, 20), "facingRight " + facingRight);
+        GUI.Label(new Rect(20, 40, 200, 20), "sr.flipX " + sr.flipX);
+        GUI.Label(new Rect(20, 60, 200, 20), "moveDir " + moveDir);
     }
 }
