462643a2f6ab5cbb16d193703e24ad8b0521c0e1
Susseroase <jacksonsking@hotmail.co.uk>
2021-02-15 14:54:19 +1100

    Revert "Revert "Merge branch 'main' of https://github.com/Badmodo/Hotdog-Sidescroller into main""
    This reverts commit a5665466d4baeb46bc1673023c7519a6052ac0bd.

---
+++ Assets/Scripts/Environment/ScoreboardTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreboardTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        UIManager.Instance?.OpenScoreBoard();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private const int MaxHealth = 3; //A const type ensure this variable can never by modified.
+    private int health;
+
+    UIManager uiManager;
+
+
+    public void TakeDamage()
+    {
+        health--;
+        if (health <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            uiManager.SetHealth(health);
+        }
+    }
+
+    void Awake()
+    {
+        //When game starts, set the player's health to max health.
+        health = MaxHealth;
+
+        //Reference UIManager
+        uiManager = UIManager.Instance;
+    }
+
+    void Update()
+    {
+        if (gameObject.transform.position.y < -6)
+        {
+            Die ();
+        }
+    }
+
+    void Die ()
+    {
+        SceneManager.LoadScene("Prototype 1");
+    }
+}
