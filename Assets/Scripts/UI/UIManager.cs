0f8e2bd8630a41dab0aaaec3ef5f293e0ceb770f
Sam <Sam@users.noreply.github.com>
2021-02-20 10:27:42 +1100

    UIManager Pause system
    Added a pause menu on Esc press

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,6 +7,9 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
 
+    public GameObject pauseMenu;
+    public bool PauseMenuOpen;
+
     ScoreBoardController scoreBoard;
     HUD hud;
 
@@ -19,14 +22,37 @@ public class UIManager : MonoBehaviour
 
 
 
-    public void OpenScoreBoard (int score)
+    public void OpenScoreBoard(int score)
     {
         scoreBoard.OpenScoreBoard(score);
     }
 
     #region HUD
-    public void SetHealth (int amount) => hud.SetHealth(amount);
+    public void SetHealth(int amount) => hud.SetHealth(amount);
     public void SetTime(int amount) => hud.SetTime(amount);
     public void SetScore(int amount) => hud.SetScore(amount);
     #endregion
+
+    void Update ()
+    {
+        if (PauseMenuOpen == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+                PauseMenuOpen = true;
+            }
+        }
+
+        else if (PauseMenuOpen == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+                PauseMenuOpen = false;
+            }
+        }
+    }
 }
