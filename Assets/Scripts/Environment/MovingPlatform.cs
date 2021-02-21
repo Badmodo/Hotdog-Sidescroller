289ffe85c0c416b6dbd4c75f30ed6bbeea96c283
Sam <Sam@users.noreply.github.com>
2021-02-21 15:47:50 +1100

    MovingPlatforms

---
+++ Assets/Scripts/Environment/MovingPlatform.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+	public GameObject TargetPoint;
+	public Vector3 origPoint;
+	float distance;
+	bool reached = false;
+	public float Speed = 0.01f;
+
+	public void Start()
+	{
+		origPoint = transform.position;
+	}
+
+	public void Update()
+	{
+		if (!reached)
+		{
+			distance = Vector3.Distance(transform.position, TargetPoint.transform.position);
+			if (distance > .1)
+			{
+				move(transform.position, TargetPoint.transform.position);
+			}
+			else
+			{
+				reached = true;
+			}
+		}
+		else
+		{
+			distance = Vector3.Distance(transform.position, origPoint);
+			if (distance > .1)
+			{
+				move(transform.position, origPoint);
+			}
+			else
+			{
+				reached = false;
+			}
+		}
+	}
+
+	void move(Vector3 pos, Vector3 towards)
+	{
+		transform.position = Vector3.MoveTowards(pos, towards, Speed);
+	}
+}
