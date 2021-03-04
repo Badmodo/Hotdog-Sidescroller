8f5188f5db233f39577becc0ba60d8b7aa60add5
Sam <samleitch75@gmail.com>
2021-03-05 10:27:22 +1100

    Added two more levels. Some enemy variation

--- Assets/Scripts/Enemy/Bullets/EnemyBullet_Basic.cs
+++ Assets/Scripts/Enemy/Bullets/EnemyBullet_Basic.cs
@@ -4,4 +4,22 @@ using System.Collections;
 public class EnemyBullet_Basic : EnemyBulletBase
 {
 
+    float aliveTimer;
+
+    public virtual void Activation()
+    {
+        StartCoroutine(DelayedAutoDespawn());
+        aliveTimer = aliveDuration;
+    }
+
+    IEnumerator DelayedAutoDespawn()
+    {
+        //Not 100% sure
+        while (aliveTimer > 0f)
+        {
+            if ((aliveTimer -= Time.deltaTime) <= 0)
+                Despawn();
+            yield return null;
+        }
+    }
 }
