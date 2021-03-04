6aab722d0705cac8381cd043b0c7ca777ba112d5
Sam <samleitch75@gmail.com>
2021-03-04 14:51:45 +1100

    TutorialsOnFirstLevel + arranged the Build Settings

---
+++ Assets/Scripts/Tutorials.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorials : MonoBehaviour
+{
+    public GameObject JumpTutorial;
+
+    void Start()
+    {
+        JumpTutorial.SetActive(true);
+    }
+
+    void Update()
+    {
+        //if(Input.GetKeyDown()
+        //{
+
+        //}
+    }
+}
